a540cba06fea72951f302948e36dfc09a2cf1ce1
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-25 05:03:04 +0700

    edit Community.js

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -2,7 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import backgroundImage from '../assets/community.jpg'
 import BackgroundBlock from '../components/BackgroundBlock'
-import { Title,Text } from '../components/Typography'
+import { Title,} from '../components/Typography'
+import { Link } from 'react-router-dom'
 
 
 const ContainerWrapper = styled.div`
@@ -31,6 +32,48 @@ const Content = styled.div`
     }
 `
 
+const ContainerBox = styled.div`
+
+
+p{
+justify-content:center;
+align-items:center;
+display:flex;
+padding:30px;
+}
+`
+const Test = styled.div`
+width: 100%;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    `
+
+const Input = styled.input`
+border: none;
+    border-bottom: 1px solid gray;
+    padding: 0 30px;
+    height: 30px;
+    width: 500px;
+    margin-top: -58px;
+    `
+
+const Button = styled.div`
+background-color: unset;
+    border: 1px solid gray;
+    border-radius: 5px;
+    padding: 10px 30px;
+    color: gray;
+    margin-top: 45px;
+    cursor: pointer;
+    :hover{
+        background-color: gray;
+        color: white;
+    }
+    `
+
 function Community (){
     return(
 
@@ -54,12 +97,20 @@ function Community (){
                 <Content>
                 <p>มีร้านคาเฟ่ที่มีใกล้มอบ้างครับ</p>
                 <p>มีร้านคาเฟ่ที่ไหนถ่ายรูปบ้างคะ</p>
-                    
-                </Content>
+            </Content>
             </Container>
-        </ContainerWrapper>
+    </ContainerWrapper>
+                
+                
+                <ContainerBox> 
+                    <p>ASK?</p>
+                </ContainerBox>
+                <Test>
+                <Input type="text" placeholder ='The message'></Input>
+                <Button><Link to='/Community'>POST</Link></Button>
+                </Test>
 
-    </>
+</>
     )
 }
 export default Community
