d9b98ae1be3c32596c33470e2034f91535f43487
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-25 00:31:20 +0700

    edit NavBar

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+
 export const Container = styled.div`
   width: 100%;
   /* position: ${props => (props.isScroll ? 'fixed' : 'static')}; */
@@ -7,22 +8,29 @@ export const Container = styled.div`
   background-color: ${props => (props.isScroll ? '#008B8B' : 'rgba(192,192,192,0.3)')};
   color:white;
   transition: background-color .5s;
+  display: flex;
+  justify-content: space-between;
+
 `
 export const Wrapper = styled.div`
+  width: 100%;
   display: flex;
   padding: 1rem;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+  /* flex: 1; */
+  justify-content: space-between;
+
 `
 export const Logo = styled.div`
   font-size: 30px;
   font-weight: 300;
+  width: 100px;
 `
 export const ActionContainer = styled.div`
+  width: 50%;
   display: flex;
-  flex: 1;
-  justify-content: flex-start;
+  justify-content: center;
+  align-items: center;
+  color: black;
   a{
   padding: 0 2rem;
   }
