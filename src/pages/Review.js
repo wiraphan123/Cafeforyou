211f5245d508f1fb418c97a102710dd8ff2cff46
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-25 05:27:34 +0700

    edit Review.js

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import backgroundImage from '../assets/reviwe.jpg'
-import backgroundImage2 from '../assets/reviwe2.jpg'
+import backgroundImage2 from '../assets/wakeup.jpg'
 import BackgroundBlock from '../components/BackgroundBlock'
 import { Title } from '../components/Typography'
 
@@ -18,6 +18,9 @@ function Review () {
       
     </BackgroundBlock>
     <BackgroundBlock src = {backgroundImage2}>
+      <Text>
+        
+      </Text>
 
     </BackgroundBlock>
     </>
