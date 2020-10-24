abe27dfeb9132d8ea00a2a3c0533657ebc3b4689
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-24 22:44:33 +0700

    edit login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,22 @@
-import React from 'react'
-function Login (){
-    return(
-    <div>Login</div>
-    )
-}
-export default Login
+// import React from 'react'
+// import { NavContainerLogin, NavButtonLogin } from './styled'
+// import { Link } from 'react-router-dom'
+// import {Text,Logo} from '../../Typography'
+// import Login from '../pages/Login'
+
+// function Navbar() {
+//   return (
+//     <NavContainerLogin>
+//       <Logo>
+//         <Link to="/home">Logo</Link>
+//         <Text>Cafe for you</Text>
+//       </Logo>
+
+//       <NavButtonLogin>
+//         <Link to="/register">สมัครสมาชิก</Link>
+//       </NavButtonLogin>
+//     </NavContainerLogin>
+//   )
+// }
+
+// export default Login
