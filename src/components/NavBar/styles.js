45ca16ce32079a12dddda9ddefe0c8d5d246f91a
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-24 16:29:54 +0700

    edit styles.js

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -3,35 +3,34 @@ export const Container = styled.div`
   width: 100%;
   /* position: ${props => (props.isScroll ? 'fixed' : 'static')}; */
   position: fixed;
-  z-index: 10000;
-  background-color: ${props => (props.isScroll ? '' : '')};
-  color: ${props => (props.isScroll ? '#DFCFAF' : '#DFCFAF')};
+  z-index: 1000;
+  background-color: ${props => (props.isScroll ? '#008B8B' : 'rgba(192,192,192,0.3)')};
+  color:white;
   transition: background-color .5s;
 `
 export const Wrapper = styled.div`
   display: flex;
-  padding: 2rem;
+  padding: 1rem;
   flex: 1;
   justify-content: center;
   align-items: center;
 `
 export const Logo = styled.div`
-  font-size: 24px;
-  font-weight: 900;
+  font-size: 30px;
+  font-weight: 300;
 `
 export const ActionContainer = styled.div`
   display: flex;
   flex: 1;
   justify-content: flex-start;
   a{
-    border-right: 1px solid #DFCFAF;
-    padding: 0 3rem;
+  padding: 0 2rem;
   }
 `
 export const Action = styled.li`
   list-style-type: none;
 `
 export const LoginContainer =styled.div`
-  font-size: 24px;
-  font-weight: 900;
+  font-size: 20px;
+  font-weight: 500;
 `
