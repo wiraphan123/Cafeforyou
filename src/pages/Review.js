eacd64f0f0e4f4da49596ee667ba36ce1880e538
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-24 16:20:16 +0700

    edit Review.js

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+import backgroundImage from '../assets/reviwe.jpg'
+import backgroundImage2 from '../assets/reviwe2.jpg'
+import BackgroundBlock from '../components/BackgroundBlock'
+import { Title } from '../components/Typography'
+
+
 function Review () {
-  return <div>Review</div>
+  return (
+    <>
+    <BackgroundBlock src = {backgroundImage}>
+
+      <Title>
+        reviwe
+      </Title>
+
+      
+    </BackgroundBlock>
+    <BackgroundBlock src = {backgroundImage2}>
+
+    </BackgroundBlock>
+    </>
+
+  )
 }
 export default Review
