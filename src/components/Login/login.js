c26f2211d2a228219e5a6e91f98864417ed82a40
pondatikan001 <pondatikan01@gmail.com>
2020-10-24 17:09:14 +0700

    add/componets/login.js

---
+++ src/components/Login/login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavContainerLogin, NavButtonLogin } from './styled'
+import { Link } from 'react-router-dom'
+import {
+  Text,
+  Logo
+} from '../../Typography'
+import Login from '../../pages/Login'
+
+function Navbar() {
+  return (
+    <NavContainerLogin>
+      <Logo>
+        <Link to="/home">Logo</Link>
+        <Text>Cafe for you</Text>
+      </Logo>
+
+      <NavButtonLogin>
+        <Link to="/register">สมัครสมาชิก</Link>
+      </NavButtonLogin>
+    </NavContainerLogin>
+  )
+}
+
+export default Login;
