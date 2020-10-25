197f65611a831e6de4ccc9cb69e9878b27af8f93
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-26 00:54:06 +0700

    edit Review

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -29,7 +29,7 @@ function Review () {
     <BackgroundBlock src = {backgroundImage}>
 
       <Title>
-        reviwe
+        review
       </Title>
       </BackgroundBlock>
       <ReviewBackground>
