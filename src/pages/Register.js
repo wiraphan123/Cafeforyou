e97212f6a8249999f26c99d7bed4d6e5ae0621f2
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-25 12:04:09 +0700

    edit Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,12 +3,14 @@ import styled from 'styled-components'
 import backgroundImage from '../assets/login-register.jpg'
 import { Link } from 'react-router-dom'
 
+
 const  Containerleft = styled.div`
     img{
         width: 50vw;
         height: 100vh;
     }
 `
+
 const Containerright = styled.div`
     width: 100%;
     height: 100%;
@@ -28,6 +30,7 @@ const Test = styled.div`
     flex-direction: row;
 `
 
+
 const Input = styled.input`
     border: none;
     border-bottom: 1px solid gray;
@@ -36,6 +39,7 @@ const Input = styled.input`
     width: 400px;
     margin-top: 75px;
 `
+
 
 const Button = styled.button`
     background-color: unset;
@@ -50,6 +54,7 @@ const Button = styled.button`
         color: white;
     }
 `
+
 
 function Register (){
     return(
