f5f17eb7935159470a507dcc182f19add42199c9
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-26 00:36:52 +0700

    edit index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import useScroll from '../../utils/useScroll'
 import { Link } from 'react-router-dom'
-import { Container, Wrapper, Logo, ActionContainer, Action, LogoImage} from './styles'
-import LogoImg from '../../assets/Weblogo.png'
+import { Container, Wrapper, ActionContainer, Action, LogoImage} from './styles'
+import Background from '../../assets/logohome.png'
+import styled from 'styled-components'
+import { useEffect } from 'react'
+
+const Logo = styled.div`
+  width: 50px;
+  height: 50px;
+  background-image: url(${Background});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`
 
 function NavBar() {
   const isScroll = useScroll({scrollRanger: 20})
   return (
     <Container isScroll={isScroll}>
       <Wrapper>
-        <LogoImage>
-          <img scr={LogoImg}/>
-        </LogoImage>
+          <Logo />
         <ActionContainer>
           <Action>
             <Link to='/'>Home</Link>
